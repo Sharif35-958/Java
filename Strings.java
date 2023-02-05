be4ee9054d5944b708b94b5de9a5f4c5695ed9f5
Md Sharifuzzaman Sharif <105845187+Sharif35-958@users.noreply.github.com>
2023-02-05 18:08:52 +0600

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Strings {
+    public static void main(String[] args) {
+//        String name = new String("Sharif");
+//        System.out.print("The Name is : ");
+//        System.out.println(name);
+        Scanner sc= new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+        int a=8;
+        float b=7.443f;
+        System.out.println("The value of a is "+a+ " and the value of b is "+b);
+        System.out.format("The value of a is "+a+ " and the value of b is "+b);
+        //System.out.printf("The value of a is %d and the value of b is %f",a,b);
+    }
+}
