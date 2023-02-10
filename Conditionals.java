6c065299275e82a17da646475706d5ae640977d5
Md Sharifuzzaman Sharif <105845187+Sharif35-958@users.noreply.github.com>
2023-02-11 02:13:34 +0600

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int age=16;
+        if(age<18){
+            System.out.println("No You cannot drive yet!");
+        }
+        else{
+            System.out.println("Yes You can drive");
+        }
+    }
+}
